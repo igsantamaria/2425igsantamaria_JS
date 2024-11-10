dbef21b0504c955582001d851724e8e4e42f55df
igsantamaria <igsantamaria@birt.eus>
2024-11-10 17:45:10 +0100

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,5 +1,11 @@
-   var color = prompt("Introduce un color para el fondo (por ejemplo: 'red', 'blue', '#ff5733'):");
-    
-document.body.style.backgroundColor = red;
 
+    alert("¡Hola, bienvenido a Hongi Etorri!");
+
+    document.body.style.backgroundColor = "lightblue";
+
+ 
+            const num1 = parseFloat(document.getElementById("num1").value);
+            const num2 = parseFloat(document.getElementById("num2").value);
+            const resultado = sumarNumeros(num1, num2);
+            document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 
