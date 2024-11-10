fcdb8142378e9e8ab8a026b8293f3032bb157b91
igsantamaria <igsantamaria@birt.eus>
2024-11-10 18:05:27 +0100

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,11 +1,29 @@
 
-    alert("¡Hola, bienvenido a Hongi Etorri!");
+// Función para la index
+function indexFuncion() {
+    console.log("Función de la página 4");
+    alert("¡Hola, bienvenido a Hongi Etorri!")
+}
 
+// Función para la página 1
+function pagina1PSFuncion() {
+    console.log("Función de la página 1");
+    document.body.style.backgroundColor = "lightgreen";
+}
+
+// Función para la página 2
+function pagina2PSFuncion() {
+    console.log("Función de la página 2");
     document.body.style.backgroundColor = "lightblue";
+}
 
- 
-            const num1 = parseFloat(document.getElementById("num1").value);
-            const num2 = parseFloat(document.getElementById("num2").value);
-            const resultado = sumarNumeros(num1, num2);
-            document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+// Función para la página 3
+function pagina3PSFuncion() {
+    console.log("Función de la página 3");
+    document.body.style.backgroundColor = "lightcoral";
+}
 
+// Función para la página 4
+function pagina4PSFuncion() {
+    console.log("Función de la página 4");
+    document.body.style.backgroundColor = "lightgoldenrodyellow";
